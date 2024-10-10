56cc80713adaa8e780d825d2308b17f47a0eb0b5
tristan <tdbrester@gmail.com>
2024-10-10 15:03:23 -0700

    completed lesson

--- src/string_methods_examples/Main.java
+++ src/string_methods_examples/Main.java
@@ -3,16 +3,27 @@ package string_methods_examples;
 public class Main {
     public static void main(String[] args) {
         //String with constructor vs string literal
+        String myFirstName = new String("Tristan");
+        String myLastName = "Brester";
+        String anotherFirstName = new String("Tristan");
+        String anotherLastName = "Brester";
+
+        System.out.println(myFirstName == anotherFirstName);
+        System.out.println(myLastName == anotherLastName);
+
+        //to compare two objects, use the .equals() method
+        System.out.println(myFirstName.equals(anotherFirstName));
 
         //String length
+        System.out.println(myFirstName.length());
 
         //substring method
+        System.out.println(myFirstName.substring(0,5));
 
         //indexOf
-
-        //equals method
+        System.out.println(myFirstName.indexOf("t"));
 
         //compareTo
-
+        System.out.println(myFirstName.compareTo(anotherFirstName));
     }
 }
